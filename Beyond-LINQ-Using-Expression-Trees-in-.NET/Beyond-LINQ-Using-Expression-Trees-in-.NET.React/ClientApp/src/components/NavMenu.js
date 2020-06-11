0037bd42291d4f8f794ebf567725364f2dabe249
Helmoz <helmo.designer1@gmail.com>
2020-06-11 10:44:42 +0300

    Исправил заголовки приложения

--- Beyond-LINQ-Using-Expression-Trees-in-.NET/Beyond-LINQ-Using-Expression-Trees-in-.NET.React/ClientApp/src/components/NavMenu.js
+++ Beyond-LINQ-Using-Expression-Trees-in-.NET/Beyond-LINQ-Using-Expression-Trees-in-.NET.React/ClientApp/src/components/NavMenu.js
@@ -27,7 +27,7 @@ export class NavMenu extends Component {
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
           <Container>
-            <NavbarBrand tag={Link} to="/">Beyond_LINQ_Using_Expression_Trees_in_.NET.React</NavbarBrand>
+            <NavbarBrand tag={Link} to="/">Beyond LINQ: Using Expression Trees in .NET</NavbarBrand>
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
